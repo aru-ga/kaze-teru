59b55a7aea6976fa8f5610837b9ae443467c255d
aruga <mfargazzy@gmail.com>
2023-07-05 11:09:14 +0700

    add tailwindcss remove cdn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./.{html,js}'],
-    theme: {},
-};
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
