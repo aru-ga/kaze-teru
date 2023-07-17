0930b87c5a4a658a22b42237ed21e9f78d1aa1ad
aruga <mfargazzy@gmail.com>
2023-07-17 11:15:24 +0700

    card adj

--- src/script.js
+++ src/script.js
@@ -91,16 +91,9 @@ navmob.addEventListener('click', () => {
   }
 });
 
-var myRadios = document.getElementsByName('tabs2');
-var setCheck;
-var x = 0;
-for(x = 0; x < myRadios.length; x++){
-  myRadios[x].onclick = function(){
-    if(setCheck != this){
-      setCheck = this;
-    }else{
-    this.checked = false;
-    setCheck = null;
-  }
- };
-}
+window.addEventListener('load', function () {
+  const preloader = document.querySelector('.preloader');
+  const mainContent = document.querySelector('.main-content');
+  preloader.classList.add('hidden');
+  mainContent.classList.remove('hidden');
+});
