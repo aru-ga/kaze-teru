ac3a0c4b4e647d3e9e9803821f418c9269d31eda
aruga <mfargazzy@gmail.com>
2023-07-20 10:39:29 +0700

    theme
    fixed text and minor

--- src/script.js
+++ src/script.js
@@ -69,16 +69,16 @@ window.addEventListener('scroll', () => {
       navbar.classList.add('border-b-2');
       navbar.classList.add('border-cyan-200');
       // hero.style.marginTop = `${navbarHeight}px`;
-      navbar.classList.add('bg-slate-900')
-      navLeftSide.classList.add('pl-10');
-      navRightSide.classList.add('pr-10');
+      navbar.classList.add('bg-slate-200')
+      navbar.classList.add('dark:bg-slate-900')
       navbar.classList.add('py-4');
       navbar.classList.add('px-10');
     } else {
       navbar.classList.remove('sticky');
       navbar.classList.remove('border-b-2');
       navbar.classList.remove('border-cyan-200');
-      navbar.classList.remove('bg-slate-900');
+      navbar.classList.remove('bg-slate-200');
+      navbar.classList.remove('dark:bg-slate-900');
       navbar.classList.remove('py-4');
       navbar.classList.remove('px-10');
     }
